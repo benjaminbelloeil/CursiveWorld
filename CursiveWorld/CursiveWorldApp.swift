42e286b94b881ff2ad0c5dbbd07e48b1bb4967bd
Benjamin Belloeil <benjaminbelloeil@outlook.com>
2025-12-03 10:26:55 +0100

    Initial Commit

---
+++ CursiveWorld/CursiveWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursiveWorldApp.swift
+//  CursiveWorld
+//
+//  Created by Benjamin Belloeil on 03/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CursiveWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
